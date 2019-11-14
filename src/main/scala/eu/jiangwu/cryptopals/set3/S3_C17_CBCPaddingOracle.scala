4eea41b838a2c23e24f2419ce013f343bc1c1fec
JWuCines <contact@jiangwu.eu>
2019-11-14 18:20:34 +0100

    WIP: S3_C17 second version

--- src/main/scala/eu/jiangwu/cryptopals/set3/S3_C17_CBCPaddingOracle.scala
+++ src/main/scala/eu/jiangwu/cryptopals/set3/S3_C17_CBCPaddingOracle.scala
@@ -3,14 +3,14 @@ package set3
 
 import eu.jiangwu.cryptopals.set1.S1_C1_HexToBase64
 import eu.jiangwu.cryptopals.set1.S1_C2_FixedXOR._
-import eu.jiangwu.cryptopals.set2.{CBCOracle, S2_C15_PCKS7RemovePadding}
+import eu.jiangwu.cryptopals.set2.{CBCOracle, S2_C15_PCKS7RemovePadding, S2_C9_PCKS7Padding}
 
 import scala.collection.mutable.ArrayBuffer
 import scala.util.Random
 
 class CBCPaddingOracle extends CBCOracle {
   val strings: List[String] = readListFromResource("set3/17.txt")
-  override def getData(data: Array[Byte]): Array[Byte] = S1_C1_HexToBase64.base64Decode(strings(Random.nextInt(10000) % strings.size))
+  override def getData(data: Array[Byte]): Array[Byte] = strings(Random.nextInt(10000) % strings.size).getBytes
   def decryptAndCheckPadding(data: Array[Byte], iv: Array[Byte]): Boolean = S2_C15_PCKS7RemovePadding.isValidPadding(decrypt(data, iv))
 }
 
@@ -19,40 +19,37 @@ object S3_C17_CBCPaddingOracle {
     val oracle: CBCPaddingOracle = new CBCPaddingOracle
     val encData: Array[Byte] = oracle.encrypt()
     Console.println("Encrypted data: " + encData.toHexString)
-    val decData: Array[Byte] = decrypt(encData, oracle)
-    Console.println("Decrypted data, knowing iv: " + decData.toCharString)
+    val decData: String = attackPaddingOracle(encData, oracle).toCharString
+    Console.println("Decrypted data, knowing iv: " + decData)
+    Console.println("Decoded and decrypted data, knowing iv: " + S1_C1_HexToBase64.base64Decode(decData))
   }
 
-  def lastBlockPreviousByte(encData: Array[Byte], oracle: CBCPaddingOracle, kIV: Array[Byte]): (Byte, Byte) = {
-    val kIVlength = kIV.length + 1
-    val prefix = generateRandomArrayByte(AES_LENGTH - kIVlength)
-    (0 until 128).map(_.toByte).foreach { i =>
-      val data: Array[Array[Byte]] = encData.grouped(AES_LENGTH).toArray
-      val gIV: Array[Byte] = if(encData.size > 16) data.dropRight(1).last else oracle.iv
-      val gVal: Array[Byte] = prefix ++ Array(i) ++ kIV.map(xor(_, kIVlength.toByte))
-      val sVal: Array[Byte] = data.dropRight(2).flatten ++ gVal ++ data.last
-      Console.println("Length kIV = " + kIVlength + ", i = " + i.toString + ", padding = " + oracle.decryptAndCheckPadding(sVal, gIV))
-      if(oracle.decryptAndCheckPadding(sVal, gIV)) {
-        val ivPrev: Byte = xor(i, kIVlength.toByte)
-        val pPrev: Byte = xor(gIV.apply(gIV.size - kIVlength), ivPrev)
-        return (ivPrev, pPrev)
+  def forcePreviousBlock(iv: Array[Byte], b: Byte, padLen: Int, kPlain: Array[Byte]): Array[Byte] = {
+    def calcChar(i: Int, by: Byte): Byte = xor(xor(iv(i), by), padLen.toByte)
+
+    val indexChar = iv.length - padLen
+    iv.take(indexChar) ++ Array(calcChar(indexChar, b)) ++
+      (AES_LENGTH - padLen + 1 until AES_LENGTH).zipWithIndex.map( v => calcChar(v._1, kPlain(v._2)))
+  }
+
+  def attackPaddingOracle(encData: Array[Byte], oracle: CBCPaddingOracle): Array[Byte] = {
+    S2_C9_PCKS7Padding.removePadding(
+      (Array(oracle.iv) ++ encData.grouped(AES_LENGTH)).sliding(2).flatMap { case blocks: Array[Array[Byte]] =>
+      val plainBlock: ArrayBuffer[Byte] = ArrayBuffer.empty[Byte]
+      (0 until AES_LENGTH).reverse.toArray.foreach { i =>
+        val padLen = plainBlock.size + 1
+        val v = (0 until 128).map(_.toByte).toArray.flatMap { c =>
+          if (oracle.decryptAndCheckPadding(blocks(1), forcePreviousBlock(blocks(0), c, padLen, plainBlock.toArray))) Option(c)
+          else None
+        }.flatMap { b =>
+          (0 until 128).map(_.toByte).flatMap { c =>
+            if (oracle.decryptAndCheckPadding(blocks(1), forcePreviousBlock(blocks(0), c, padLen + 1, Array(b) ++ plainBlock))) Option(b)
+            else None
+          }
+        }
+        plainBlock.prepend(v.head)
       }
-    }
-    (0, 0)
-  }
-
-  def lastBlock(encData: Array[Byte], oracle: CBCPaddingOracle): Array[Byte] = {
-    val kIV: ArrayBuffer[Byte] = ArrayBuffer.empty[Byte]
-    (0 until AES_LENGTH).map { _ =>
-      val (i, t) = lastBlockPreviousByte(encData, oracle, kIV.toArray.reverse)
-      kIV.append(i)
-      t
-    }.toArray
-  }
-
-  def decrypt(encData: Array[Byte], oracle: CBCPaddingOracle): Array[Byte] = {
-    (0 until (encData.length / AES_LENGTH)).map { i =>
-      lastBlock(encData.dropRight(i * AES_LENGTH), oracle)
-    }.reverse.flatten.toArray
+      plainBlock
+    }.toArray)
   }
 }
